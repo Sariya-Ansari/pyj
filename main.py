f8eba92f91b3d97b8ade393d0f76bc3a53a5e876
Sariya-Ansari <sariyapersonal@gmail.com>
2024-09-17 17:46:52 +0530

    Initial Code of Open Source PyJ
    Added 
     - Added jcollections initial code
     - Added jstring initial code
     - Added api test 
     - Added unit test
     -

---
+++ main.py
@@ -0,0 +1,20 @@
+from apitest.api_usage_test import test_string, test_arraylist, test_linkedlist, test_vector, test_stack, test_queue, \
+    test_priorityQ, test_deque, test_hashset, test_linked_hash_set, test_tree_set, test_hashmap, test_linked_hash_map, \
+    test_hashtable, test_treemap
+
+if __name__ == '__main__':
+    test_string()
+    test_arraylist()
+    test_linkedlist()
+    test_vector()
+    test_stack()
+    test_queue()
+    test_priorityQ()
+    test_deque()
+    test_hashset()
+    test_linked_hash_set()
+    test_tree_set()
+    test_hashmap()
+    test_linked_hash_map()
+    test_hashtable()
+    test_treemap()
